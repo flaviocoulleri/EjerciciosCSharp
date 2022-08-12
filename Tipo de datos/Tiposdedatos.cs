19f8cf11c661fce2fc820f162cb3d82ed13b1c10
flaviocoulleri <flavionicolascoulleri@gmail.com>
2022-08-12 00:14:26 -0300

    Tipos de datos

---
+++ Tipo de datos/Tiposdedatos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace tipos_datos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 19;
+            string nombre = "Flavio Coulleri", curso = "C#";
+
+            Console.WriteLine("Mi nombre es: " + nombre);
+            Console.WriteLine("Edad: " + edad);
+            Console.WriteLine("Curso: " + curso);
+
+            Console.ReadKey();
+        }
+    }
+}
