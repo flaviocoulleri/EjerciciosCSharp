908f82b693bc18eb2b49e30ea606368811776444
flaviocoulleri <flavionicolascoulleri@gmail.com>
2022-08-12 00:14:18 -0300

    Ciclos

---
+++ Ciclos for/Ciclos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ciclos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic suma=0, resta=0, multiplicacion=1, promedio=0;
+            double num;
+            int cont;
+            
+            for(cont=0;cont<10;cont++)
+            { Console.WriteLine("Ingrese un numero: ");
+                num = Convert.ToDouble(Console.ReadLine());
+                suma += num;
+                resta -= num;
+                multiplicacion *= num;
+            }
+            promedio = suma / 10;
+            Console.WriteLine("La suma total es: "+suma);
+            Console.WriteLine("La resta total es: " +resta);
+            Console.WriteLine("La multiplicacion total es: " +multiplicacion);
+            Console.WriteLine("El promedio total es: " +promedio);
+            Console.ReadKey();
+        }
+    }
+}
