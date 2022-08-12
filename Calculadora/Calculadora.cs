3a40c3d5f2f3f6aa8108d52c40d78439ad5ff4e5
Flavio Coulleri <104596145+flaviocoulleri@users.noreply.github.com>
2022-08-12 00:01:48 -0300

    Calculadora

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, operacion;
+            dynamic resu;
+
+            Console.WriteLine("Ingrese el primer numero: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo numero: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la operacion que desea realizar:\n1)Suma 2)Resta 3)Multiplicacion 4)Division");
+            operacion = Convert.ToDouble(Console.ReadLine());
+            
+            switch(operacion)
+            { case 1:
+                    resu = num1 + num2;
+                    Console.WriteLine("El resultado es: "+resu);
+                    break;
+                case 2:
+                    resu = num1 - num2;
+                    Console.WriteLine("El resultado es: " + resu);
+                    break;
+                case 3:
+                    resu = num1 * num2;
+                    Console.WriteLine("El resultado es: " + resu);
+                    break;
+                case 4:
+                    resu = num1 / num2;
+                    Console.WriteLine("El resultado es: " + resu);
+                    break;
+            }
+            Console.ReadKey();
+        }   
+    }
+}
